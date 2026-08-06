5747a36d21aeba18c7161a180fbca78957cce839
MiracleCodez <151534060+MiracleCodez@users.noreply.github.com>
2026-08-06 13:08:09 +0200

    Add useRef import to EventDetailScreen.js

--- gentsconcerts-app/screens/EventDetailScreen.js
+++ gentsconcerts-app/screens/EventDetailScreen.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { Platform } from 'react-native';
 import { 
   View, Text, StyleSheet, ScrollView, TouchableOpacity, 
